03eb2573cf98051a73fd4197d0f2698c8e7ed96a
Jon Miller <jonmiller@Jons-MacBook-Pro.local>
2022-04-27 14:26:28 -0700

    fixed navabar mobile spacing issues

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto max-w-6xl">
-        <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+        <span className="self-center text-lg sm:text-xl font-semibold whitespace-nowrap text-white">
           <Link href="/" className="flex items-center cursor-pointer">
             CHEAPER INSURANCE NEVADA
           </Link>
@@ -17,7 +17,7 @@ function Navbar() {
           <Link href="/form">
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 sm:px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Get a quote
             </button>
