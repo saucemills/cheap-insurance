c3f50e08770d1b91954b071ba0f3aab78551c5af
Jon Miller <jonmiller@Jons-MacBook-Pro.local>
2022-04-27 18:18:09 -0700

    fix footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -6,11 +6,11 @@ export default function Footer() {
   return (
     <div className="relative bottom-0 w-full h-10 bg-gray-800 border-gray-200 px-2 sm:px-4 py-2.5">
       <div className="container flex flex-wrap justify-center items-center mx-auto max-w-6xl">
-        <a href="https://flowbite.com" className="flex items-center">
+        <h2 className="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
             Cheaper Insurance Nevada
           </span>
-        </a>
+        </h2>
       </div>
     </div>
   );
