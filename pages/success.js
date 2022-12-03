bf2227034215c47aa9164097a9855807ae623a8f
Jon Miller <jonmiller@Jons-MacBook-Pro.local>
2022-12-02 19:44:23 -0800

    added call button to success screen

--- pages/success.js
+++ pages/success.js
@@ -30,13 +30,27 @@ function success() {
                 A specialist will contact you within the next 24-48 hours with
                 your cheapest possible quote.
               </p>
-              <button
-                type="button"
-                onClick={handleClick}
-                className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-              >
-                Back Home
-              </button>
+              <a className="flex justify-center mb-2" href="tel:7023897341">
+                <button className="outline-none bg-blue-700 hover:bg-blue-800 text-white text-center px-4 py-3 rounded-md shadow w-1/2 ring-offset-2 ring-gray-700 focus:ring-2  sm:w-auto">
+                  <div className="flex justify-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 mr-1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                      />
+                    </svg>
+                    Call Us Now!
+                  </div>
+                </button>
+              </a>
             </div>
           </div>
         </div>
