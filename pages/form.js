c7abcced9ef5b46739709594f07e1fa769a72f56
Jon Miller <jonmiller@Jons-MacBook-Pro.local>
2022-12-04 11:33:47 -0800

    fixed head error on form.js

--- pages/form.js
+++ pages/form.js
@@ -12,18 +12,13 @@ export default function form() {
   return (
     <>
       <Head>
-        <Head>
-          <title>Cheaper Car Insurance NV</title>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta
-            name="description"
-            content="Find out if you can get a better rate on your car insurance in NV. Answer a few simple questions and get a quote for your car insurance to save money!"
-          />
-        </Head>
+        <title>Cheaper Car Insurance NV</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Find out if you can get a better rate on your car insurance in NV. Answer a few simple questions and get a quote for your car insurance to save money!"
+        />
       </Head>
       <main className="h-screen m-0">
         <div className="min-h-full mb-[-40px]">
